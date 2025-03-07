129a9448c7dd10b78bb87b2e59e349c9188c1949
Playeira01 <mcarmencd00@gmail.com>
2025-03-07 23:23:30 +0100

    feat: add chat  history  #20

--- frontend/app/src/screens/IndividualChatScreen.tsx
+++ frontend/app/src/screens/IndividualChatScreen.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useCallback } from 'react';
-import { View, StyleSheet} from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, StyleSheet, Button, Image, TouchableOpacity } from 'react-native';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+
 
 const API_URL = "http://localhost:3000/messages"; // URL del JSON Server
 
@@ -14,7 +15,37 @@ const artistAvatar= "https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-12953
 export default function IndividualChatScreen({ navigation }) {
     
     const [messages, setMessages] = useState<IMessage[]>([]);
-          
+    
+      {/*Obtención del historial del chat*/}
+      useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                {/*Solicitud al backend*/}
+                const response = await fetch(API_URL);
+                const data = await response.json();
+
+                {/*Formateo del json del backend al json que necesita el componente GiftedChat  */}
+                const formattedMessages: IMessage[] = data.map((msg: any) => ({
+                    _id: msg.id,
+                    text: msg.text,
+                    createdAt: new Date(msg.createdAt), 
+                    user: {
+                        _id: msg.user.id,
+                        name: msg.user.name,
+                        avatar: msg.user.avatar || artistAvatar,
+                    },
+                })).sort((a: IMessage, b: IMessage) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+
+                setMessages(formattedMessages);
+            } catch (error) {
+                console.error("Error obteniendo mensajes:", error);
+            }
+        };
+
+        fetchMessages();
+    }, []);
+
+
     {/*Publicación de los mensajes*/}
     const onSend = useCallback(async (newMessages: IMessage[] = []) => {
 
@@ -45,7 +76,7 @@ export default function IndividualChatScreen({ navigation }) {
             },
         };
     
-        {/*Envía los mensajes al "backend" */}
+        {/*Envia los mensajes al "backend" */}
         try {
             const response = await fetch(API_URL, {
                 method: "POST",
