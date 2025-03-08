cd51d77a187c775b7679b77561f0864f32c7be2a
jose-maria-portela <92798583+jose-maria-portela@users.noreply.github.com>
2025-03-08 16:32:32 +0100

    feat: Added funtionality, but not working yet

--- Backend/src/main/java/com/HolosINC/Holos/security/SecurityConfig.java
+++ Backend/src/main/java/com/HolosINC/Holos/security/SecurityConfig.java
@@ -1,19 +1,43 @@
 package com.HolosINC.Holos.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:8080"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                config.setAllowedHeaders(List.of("*"));
+                return config;
+            }))		
+			.csrf(AbstractHttpConfigurer::disable)		
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))			
+			.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable()))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/search/**").permitAll() // 🔹 Permite acceso a la búsqueda sin login
-                .anyRequest().authenticated() // 🔒 El resto de la API sigue protegida
+                .requestMatchers("/api/v1/baseUser/signin").anonymous()
+                .anyRequest().permitAll() // 🔒 El resto de la API sigue protegida
             )
             .csrf(csrf -> csrf.disable()); // Opcional: Desactivar CSRF si no usas formularios
 
