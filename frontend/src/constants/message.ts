63b874bb91982704215163b836f1fdde083e2d33
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-04-27 18:06:06 +0200

    fix: chat again

---
+++ frontend/src/constants/message.ts
@@ -0,0 +1,21 @@
+
+import { StatusCommission } from "./CommissionTypes";
+
+export interface MessageChat {
+  text: string,
+  createdAt: string,
+  image: string | undefined,
+  commision: string | undefined
+}
+
+
+export interface MessageRecieved {
+  id: number, 
+  text: string,
+  creationDate: string,
+  image: string | undefined,
+  commisionId: number,
+  senderId: number,
+  senderName: string,
+  statusCommision: StatusCommission
+}
